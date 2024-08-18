9bcc0dbcaa1726dafce30258bd915feb54a4e4b6
pc <pc@DESKTOP-M7QJQNL>
2024-08-18 09:12:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+
+
+List<string> isimler = new List<string>();
+
+
+Console.WriteLine("EN SEVDİĞİN 0 5 KAHVE.. BAŞLIYORUZ");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"En sevdiğiniz {i + 1}. kahve çeşidi");
+    string kahve = Console.ReadLine();
+    isimler.Add(kahve);
+}
+
+Console.WriteLine("İşte o favori kahvelerin aşşağıda \n");
+foreach (string item in isimler)
+{
+    Console.WriteLine(item);
+}
